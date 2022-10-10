fb4f50dc70d6460c8b1bac81c70fe94b60292494
Xwyzworms <pratamaazmi1@gmail.com>
2022-10-10 10:20:18 +0700

    change Adapter implementation

--- app/src/main/java/com/devtedi/tedi/adapter/SectionPagerAdapter.kt
+++ app/src/main/java/com/devtedi/tedi/adapter/SectionPagerAdapter.kt
@@ -9,25 +9,24 @@ import com.devtedi.tedi.presentation.feature_sign_language.SignLanguageFragment
 import com.devtedi.tedi.presentation.feature_text.TextDetectionFragment
 
 
-class SectionPagerAdapter (activity : AppCompatActivity) : FragmentStateAdapter(activity) {
+class SectionPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
         return TOTAL
     }
 
     override fun createFragment(position: Int): Fragment {
-        var fragment : Fragment?= null
-        when(position) {
-            0 -> fragment = SignLanguageFragment()
-            1 -> fragment = ObjectDetectionFragment()
-            2 -> fragment = CurrencyFragment()
-            3 -> fragment = TextDetectionFragment()
+        val fragment: Fragment? = when (position) {
+            0 -> ColorDetectorFragment()
+            1 -> SignLanguageFragment()
+            2 -> CurrencyFragment()
+            3 -> ColorDetectorFragment()
+            4 -> ColorDetectorFragment()
+            else -> null
         }
         return fragment as Fragment
     }
 
-    companion object
-    {
-        private const val TOTAL : Int = 4
-
+    companion object {
+        private const val TOTAL: Int = 5
     }
 }
