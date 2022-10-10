01a07d5bb34637e2752811143124fe8572737173
Xwyzworms <pratamaazmi1@gmail.com>
2022-10-10 10:29:10 +0700

    add core abstraction, and core model

---
+++ app/src/main/java/com/devtedi/tedi/core_model/BisindoTranslator.kt
@@ -0,0 +1,14 @@
+package com.devtedi.tedi.core_model
+
+import android.util.Size
+
+class BisindoTranslator (
+    override var inputSize: Size,
+    override var outputSize: IntArray,
+    override var detect_threshold: Float,
+    override var IOU_threshold: Float,
+    override var IOU_class_duplicated_threshold: Float,
+    override var label_file: String,
+    override var model_file: String,
+    override var IS_INT_8: Boolean,
+        ) : ObjectDetectorType()
