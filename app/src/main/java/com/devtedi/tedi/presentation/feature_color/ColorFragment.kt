ffc7bddedd285ce89f085112513db1eb808efe1d
ekotyoo <ekeprasetyo@gmail.com>
2022-12-30 18:34:00 +0700

    docs: write docs for ColorFragment.kt

--- app/src/main/java/com/devtedi/tedi/presentation/feature_color/ColorFragment.kt
+++ app/src/main/java/com/devtedi/tedi/presentation/feature_color/ColorFragment.kt
@@ -1,8 +1,6 @@
 package com.devtedi.tedi.presentation.feature_color
 
-import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +10,7 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.devtedi.tedi.R
 import com.devtedi.tedi.analyzer.ColorDetectionAnalyzer
 import com.devtedi.tedi.databinding.FragmentColorBinding
@@ -19,20 +18,27 @@ import com.devtedi.tedi.factory.ColorGenerator
 import com.devtedi.tedi.utils.SoundPlayer
 import com.devtedi.tedi.utils.scaleBitmapDown
 import org.opencv.android.OpenCVLoader
-import java.io.ByteArrayOutputStream
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+/**
+ *
+ * Fragment untuk deteksi warna menggunakan Machine Learning
+ *
+ * @property binding view binding dari ColorFragment
+ * @property colorAnalyzer analyzer warna untuk cameraX
+ * @constructor untuk buat instance dari CloudModelAdapter.
+ */
 class ColorFragment : Fragment() {
 
-    private lateinit var binding : FragmentColorBinding
-    private lateinit var colorAnalyzer : ColorDetectionAnalyzer
+    private lateinit var binding: FragmentColorBinding
+    private lateinit var colorAnalyzer: ColorDetectionAnalyzer
     private lateinit var colorGenerator: ColorGenerator
-    private lateinit var cameraExecutor : ExecutorService
-    private var soundPlayer : SoundPlayer ?= null
+    private lateinit var cameraExecutor: ExecutorService
+    private var soundPlayer: SoundPlayer? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentColorBinding.inflate(inflater)
         return binding.root
@@ -50,8 +56,8 @@ class ColorFragment : Fragment() {
         super.onResume()
         startCamera()
     }
-    private fun initConfiguration()
-    {
+
+    private fun initConfiguration() {
         cameraExecutor = Executors.newSingleThreadExecutor()
         colorAnalyzer = ColorDetectionAnalyzer(requireContext())
     }
@@ -60,8 +66,7 @@ class ColorFragment : Fragment() {
         binding.btnCapture.setOnClickListener {
             val image = colorAnalyzer.getDetectedImage()
             soundPlayer?.playSound(requireContext().resources.getString(R.string.raw_sound_take_pict))
-            if (image != null)
-            {
+            if (image != null) {
                 val currImage = scaleBitmapDown(image, maxDimension)
                 colorGenerator = ColorGenerator(requireActivity(), currImage)
             }
@@ -69,6 +74,7 @@ class ColorFragment : Fragment() {
 
     }
 
+    // Setup camera untuk analisis warna, for more details see CameraX docs.
     private fun startCamera() {
         try {
             val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
@@ -100,14 +106,15 @@ class ColorFragment : Fragment() {
                 runnableInterface,
                 ContextCompat.getMainExecutor(requireContext())
             )
-        }
-        catch (e : UninitializedPropertyAccessException)
-        {
-            Toast.makeText(context, "Mohon tunggu sebentar, fitur sedang disiapkan", Toast.LENGTH_SHORT).show()
+        } catch (e: UninitializedPropertyAccessException) {
+            Toast.makeText(context,
+                "Mohon tunggu sebentar, fitur sedang disiapkan",
+                Toast.LENGTH_SHORT).show()
         }
     }
+
     companion object {
-        private const val maxDimension : Int = 640
+        private const val maxDimension: Int = 640
     }
 
 
